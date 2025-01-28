2985803c6df8673e97ce1d9922e58ae5f3e74bfd
Tyler Sahagun <tylersahagun@gmail.com>
2025-01-28 15:34:20 -0700

    Initial Commit

---
+++ Landmarks/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Landmarks
+//
+//  Created by Tyler Sahagun on 1/28/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
